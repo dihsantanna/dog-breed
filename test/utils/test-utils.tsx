30aba48d9ae6ebf1b5a09da958df0faf90bcb7e6
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-08-04 14:17:31 -0300

    refactor: refatora customRender para funcionar com react-router-dom. Agora função se chama renderWithRouter

--- test/utils/test-utils.tsx
+++ test/utils/test-utils.tsx
@@ -1,17 +1,23 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { cleanup, render } from '@testing-library/react';
 import { afterEach } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 afterEach(() => {
   cleanup();
 });
 
-const customRender = (ui: React.ReactElement, options = {}) => render(ui, {
-  // wrap provider(s) here if needed
-  wrapper: ({ children }) => children,
-  ...options,
-});
+const renderWithRouter = (ui: React.ReactElement, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+  return ({
+    user: userEvent.setup(),
+    ...render(ui, {
+      wrapper: BrowserRouter,
+    }),
+  });
+};
 
 export * from '@testing-library/react';
-export { default as userEvent } from '@testing-library/user-event';
-// override render export
-export { customRender as render };
+
+export { renderWithRouter as render };
