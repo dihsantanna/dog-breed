557f0f5eac793f9308f50fdc45d39f7732b629b3
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-08-04 17:52:04 -0300

    test: inicia implementação de testes E2E para rota /register

---
+++ test/E2E/Register.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '../utils/test-utils';
+import App from '../../src/App';
+import {
+  EMAIL_INPUT, LIST_PAGE, LOGIN_BUTTON, REGISTER_PAGE,
+} from '../utils/testIds';
+
+describe.skip('Testando rota "/register"', () => {
+  it('Ao acessar rota "/" é redirecionado para rota "/register', () => {
+    const { history } = render(<App />);
+    const { pathname } = history.location();
+    const registerPage = screen.getByTestId(REGISTER_PAGE);
+    expect(pathname).toBe('/register');
+    expect(registerPage).toBeInTheDocument();
+  });
+
+  it(`Ao digitar email válido no campo de registro e clicar no botão "Entrar"
+  é redirecionado para a rota "/list"`, async () => {
+    const { user } = render(<App />);
+    const emailInput = screen.getByTestId(EMAIL_INPUT);
+    const loginButton = screen.getByTestId(LOGIN_BUTTON);
+
+    await user.click(emailInput);
+    await user.paste('usuario@email.com');
+
+    await user.click(loginButton);
+
+    const listPage = screen.getByTestId(LIST_PAGE);
+
+    expect(listPage).toBeInTheDocument();
+  });
+
+  it(`Se o usuário já estiver logado, ao acessar a rota "/register",
+  deve ser redirecionado para rota "/list"`, async () => {
+    const { user, history } = render(<App />);
+    const emailInput = screen.getByTestId(EMAIL_INPUT);
+    const loginButton = screen.getByTestId(LOGIN_BUTTON);
+
+    await user.click(emailInput);
+    await user.paste('usuario@email.com');
+
+    await user.click(loginButton);
+
+    history.push('/register');
+
+    const { pathname } = history.location();
+    const listPage = screen.getByTestId(LIST_PAGE);
+
+    expect(pathname).toBe('/list');
+    expect(listPage).toBeInTheDocument();
+  });
+});
---
+++ test/utils/testIds.ts
@@ -0,0 +1,4 @@
+export const REGISTER_PAGE = 'register-page';
+export const EMAIL_INPUT = 'email-input';
+export const LOGIN_BUTTON = 'login-button';
+export const LIST_PAGE = 'list-page';
