8aa6a1dac1769264db032d142786de80b9513340
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-08-05 20:38:03 -0300

    refactor: muda retorno de funções request e configura variaveis de ambiente para VITE

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import { IUser } from '../types/IUser';
+import { IErrorApi } from '../types/IErrorApi';
+import { IDogBreed } from '../types/IDogBreed';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const setToken = (token: string) => {
@@ -9,13 +12,13 @@ export const setToken = (token: string) => {
 };
 
 export const requestData = async (endpoint: string) => {
-  const { data } = await api.get(endpoint);
-  return data;
+  const { data, status } = await api.get(endpoint);
+  return { data: data as IDogBreed | IErrorApi, status };
 };
 
 export const requestLogin = async (endpoint: string, body: { email: string }) => {
-  const { data } = await api.post(endpoint, body);
-  return data;
+  const { data, status } = await api.post(endpoint, body);
+  return { data: data as IUser | IErrorApi, status };
 };
 
 export default api;
