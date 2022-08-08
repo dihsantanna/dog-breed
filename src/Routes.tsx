70f46a25f72b779a1476587627ba14233316f946
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-08-08 17:19:05 -0300

    feat: implementação inicial da pagina de Lista de breeds

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Routes as SwitchRoutes, Route, Navigate } from 'react-router-dom';
+import { ListPage } from './pages/ListPage';
 import { RegisterPage } from './pages/RegisterPage';
 
 export function Routes() {
   return <SwitchRoutes>
     <Route path="/" element={<Navigate to="register" replace={true} />} />
     <Route path="/register" element={<RegisterPage/>} />
-    <Route path="/list" element={<h1>List</h1>} />
+    <Route path="/list" element={<ListPage />} />
   </SwitchRoutes>;
 }
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,7 +1,87 @@
-import React from 'react';
+import jwtDecode, { InvalidTokenError } from 'jwt-decode';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { DOG_IMG_TESTID, LOGO_IMG_TESTID } from '../../test/utils/testIds';
+import { requestData, setToken } from '../services/request';
+import { IDogBreed } from '../types/IDogBreed';
+import { IError } from '../types/IErrorApi';
+
+const breeds = ['Chihuahua', 'Husky', 'Labrador', 'Pug'];
 
 export function ListPage() {
+  const [breedImages, setBreedImages] = useState<string[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [selectedBreed, setSelectedBreed] = useState('chihuahua');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  const requestBreedImages = useCallback(async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+    try {
+      const token = localStorage.getItem('token');
+
+      if (!token || jwtDecode<InvalidTokenError>(token).message) {
+        navigate('../register', { replace: true });
+        return;
+      }
+
+      setToken(token as string);
+      const { data, status } = await requestData(`/list?breed=${selectedBreed}`);
+
+      if (status !== 200) {
+        setErrorMessage((data as IError).error.message);
+        return;
+      }
+
+      setBreedImages((data as IDogBreed).list);
+    } catch (error) {
+      setErrorMessage((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [navigate, selectedBreed]);
+
+  useEffect(() => {
+    requestBreedImages();
+  }, [requestBreedImages]);
+
   return (
-    <div>ListPage</div>
+    <>
+    <div>
+        <header>
+          <img
+            data-testid={LOGO_IMG_TESTID}
+            src='src/assets/dog_breed_logo.svg'
+            alt="logo dog breed"
+            className='listPage-logo'
+          />
+        </header>
+        <div>
+          {breeds.map((breed) => (
+            <button
+            key={`${breed}-button`}
+            type="button"
+            onClick={() => setSelectedBreed(breed.toLowerCase())}
+            >{breed}</button>
+          ))}
+        </div>
+        <span>{errorMessage}</span>
+        <section>
+          {isLoading
+            ? <span>Loading</span>
+            : breedImages.map((breedImg, index) => (
+              <img
+                key={`${selectedBreed}-image-${index}`}
+                data-testid={`${DOG_IMG_TESTID}${selectedBreed}-${index}`}
+                src={breedImg}
+                alt={`Imagem de uma ${selectedBreed} ${index}`}
+              />
+            ))
+          }
+        </section>
+    </div>
+    </>
   );
 }
